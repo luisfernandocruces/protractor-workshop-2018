baeedd47f880af1934b7e68dfcae3138238ca407
luisfcv97 <luisfcv97@gmail.com>
2019-09-21 11:02:48 -0500

    fixed dependendy

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -1,8 +1,5 @@
 import { Config, browser } from 'protractor';
-<<<<<<< HEAD
 import { reporter } from './helpers/reporter';
-=======
->>>>>>> 45fb3e09ad1384bd72b5ba4e48d548e15e24ca39
 
 export const config: Config = {
   framework: 'jasmine',
@@ -10,9 +7,6 @@ export const config: Config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   onPrepare: () => {
     browser.ignoreSynchronization = true;
-<<<<<<< HEAD
     reporter();
-=======
->>>>>>> 45fb3e09ad1384bd72b5ba4e48d548e15e24ca39
   }
 };
